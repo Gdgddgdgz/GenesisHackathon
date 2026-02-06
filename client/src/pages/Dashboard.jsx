01b8ef2011463283a9fd41be7492dadc6b6ad5e5
SaketKunjathur <saketkunjathur@gmail.com>
2026-02-07 01:59:12 +0530

    fixed-endpoint

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -260,7 +260,7 @@ const Dashboard = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="h-[350px]">
+                    <div style={{ width: '100%', height: 350 }}>
                         <ResponsiveContainer width="100%" height="100%">
                             <AreaChart data={chartData}>
                                 <defs>
